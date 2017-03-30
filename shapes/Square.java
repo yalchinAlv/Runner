f88776e9308b0070af8cc16fbac32cfbd9525386
Yalchin Aliyev <yalcin9a1@live.com>
2017-03-30 23:19:35 +0300

    Add files via upload

---
+++ shapes/Square.java
@@ -0,0 +1,29 @@
+package shapes;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+/**
+ * 
+ * @author Yalchin Aliyev
+ * @version 0.01 28.02.2016
+ *
+ */
+public class Square extends Rectangle {
+
+	// properties
+	protected int side;
+	
+	// constructors
+	public Square( int side) {
+		
+		super(side, side);
+		this.side = side;
+		
+		
+	}
+
+	
+	
+	
+}
